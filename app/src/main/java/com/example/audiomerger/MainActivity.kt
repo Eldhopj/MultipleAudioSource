5d21754d786147b99f5ad0f8324e7c3403791168
Eldho P James <eldho@live.in>
2023-05-12 08:52:46 +0530

    recording in mp3 format

--- app/src/main/java/com/example/audiomerger/MainActivity.kt
+++ app/src/main/java/com/example/audiomerger/MainActivity.kt
@@ -4,97 +4,141 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.audiomerger.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import java.io.File
 
 
 @SuppressLint("MissingPermission")
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
-    private lateinit var micRecorder: MediaRecorder
-    private lateinit var headphoneRecorder: MediaRecorder
+    private var isRecording = false
+
+    private val REQUEST_PERMISSION_CODE = 1
+
+    // Initialize the MediaRecorder objects
+    private lateinit var mediaRecorderFromDeviceMic: MediaRecorder
+    private lateinit var mediaRecorderFromHeadphoneMic: MediaRecorder
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         binding.start.setOnClickListener {
-            // Check if the app has permission to record audio.
-            if (checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                // Request permission.
-                requestPermissions(arrayOf(android.Manifest.permission.RECORD_AUDIO), 100)
-            } else {
-                // Start recording.
-                initRec()
+            if (!isRecording) {
+                if (checkPermissions()) {
+                    startRecording()
+                    Toast.makeText(this, "Recording started", Toast.LENGTH_SHORT).show()
+                } else {
+                    requestPermissions()
+                }
             }
         }
 
         binding.end.setOnClickListener {
-            headphoneRecorder.stop()
-            micRecorder.stop()
-            headphoneRecorder.release()
-            micRecorder.release()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == 100) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Start recording.
-                initRec()
-            } else {
-                // Permission denied.
-                Log.e(TAG, "Permission denied to record audio.")
+            if (isRecording) {
+                stopRecording()
+                Toast.makeText(this, "Recording stopped", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun initRec() {
-        micRecorder = MediaRecorder()
-        headphoneRecorder = MediaRecorder()
-        startRecording(micRecorder, MediaRecorder.AudioSource.MIC, "recording_mic.ogg")
-        startRecording(headphoneRecorder, MediaRecorder.AudioSource.DEFAULT, "recording_headphone.ogg")
+    private fun checkPermissions(): Boolean {
+        val recordPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.RECORD_AUDIO
+        )
+        val modifySettingsPermission = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.MODIFY_AUDIO_SETTINGS
+        )
+        return recordPermission == PackageManager.PERMISSION_GRANTED &&
+                modifySettingsPermission == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun startRecording(mediaRecorder: MediaRecorder, source: Int, fileName: String) {
-        // Set the audio source to both the device mic.
-        mediaRecorder.setAudioSource(source)
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.MODIFY_AUDIO_SETTINGS
+            ),
+            REQUEST_PERMISSION_CODE
+        )
+    }
 
-        // Set the output format to .wav.
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.OGG)
-
-        // Set the output file path.
-        val filePath = "${getExternalFilesDir(Environment.DIRECTORY_MUSIC)}/$fileName"
-        mediaRecorder.setOutputFile(filePath)
-
-        // Set the audio encoder.
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-
-        // Prepare the MediaRecorder object.
-        try {
-            mediaRecorder.prepare()
-        } catch (e: Exception) {
-            Log.e(TAG, "Error preparing MediaRecorder: $e")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startRecording()
+                Toast.makeText(this, "Recording started", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
 
-        // Start recording.
-        CoroutineScope(Dispatchers.Default).launch {
-            mediaRecorder.start()
-        }
+    // Create instances of MediaRecorder to handle recording from the device microphone and the headphone microphone simultaneously
+    private fun setupAudioRecording() {
+        // Create the MediaRecorder for recording from the device microphone
+        mediaRecorderFromDeviceMic = MediaRecorder()
+        mediaRecorderFromDeviceMic.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorderFromDeviceMic.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorderFromDeviceMic.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        mediaRecorderFromDeviceMic.setOutputFile(getAudioFilePath("device_mic_audio.mp3"))
+
+        // Create the MediaRecorder for recording from the headphone microphone
+        mediaRecorderFromHeadphoneMic = MediaRecorder()
+        mediaRecorderFromHeadphoneMic.setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+        mediaRecorderFromHeadphoneMic.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorderFromHeadphoneMic.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        mediaRecorderFromHeadphoneMic.setOutputFile(getAudioFilePath("headphone_mic_audio.mp3"))
+    }
+
+    // Save the recorded audio to a file
+    private fun getAudioFilePath(fileName: String): String {
+        val audioFile = File(getExternalFilesDir(null), fileName)
+        return audioFile.absolutePath
+    }
+
+    // Start recording
+    private fun startRecording() {
+        setupAudioRecording()
+
+        isRecording = true
+        // Start recording from the device microphone
+        mediaRecorderFromDeviceMic.prepare()
+        mediaRecorderFromDeviceMic.start()
+
+        // Start recording from the headphone microphone
+        mediaRecorderFromHeadphoneMic.prepare()
+        mediaRecorderFromHeadphoneMic.start()
+    }
+
+    // Stop recording
+    private fun stopRecording() {
+        // Stop recording from the device microphone
+        isRecording = false
+        mediaRecorderFromDeviceMic.stop()
+        mediaRecorderFromDeviceMic.release()
+
+        // Stop recording from the headphone microphone
+        mediaRecorderFromHeadphoneMic.stop()
+        mediaRecorderFromHeadphoneMic.release()
     }
 
 }
